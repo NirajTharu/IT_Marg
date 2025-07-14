22f777796251cdff3bb819fa123ec86f83bce706
NirajTharu <nirajc279@gmail.com>
2025-07-14 17:53:12 +0545

    The comment about the question

--- javascript/classwork/classwork1.js
+++ javascript/classwork/classwork1.js
@@ -1,5 +1,17 @@
 {
-  // Assuming the full mark of one subject is 100;
+
+// Q.Create a simple JavaScript program that:
+
+// Declares variables for a student's name and marks in three subjects (Math, Science, English).
+
+// Calculates the total marks, average, and percentage.
+
+// Displays the student's name, total marks, average, and percentage in the console.
+
+// Use appropriate data types and operators.
+
+// Add one condition: If the percentage is 40 or more, show "Pass", otherwise show "Fail".
+//   // Assuming the full mark of one subject is 100;
   var studentName = "Niraj";
   var math = 80,
     science = 80,
