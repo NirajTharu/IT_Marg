b5d246c6a6c91a61a21fb3009136495867be2a75
NirajTharu <nirajc279@gmail.com>
2025-07-15 16:49:34 +0545

    Adding classowork

---
+++ javascript/classwork/classwork3.js
@@ -0,0 +1,28 @@
+{
+  // Switch case statements
+  let day = "sunday";
+  switch (day) {
+    case "sunday":
+      console.log("Today is sunday");
+      break;
+    case "monday":
+      console.log("Today is monday");
+      break;
+    case "tuesday":
+      console.log("Today is tuesday");
+      break;
+    case "wednesday":
+      console.log("Today is wednesday");
+      break;
+    case "thursday":
+      console.log("Today is thursday");
+      break;
+    case "friday":
+      console.log("Today is friday");
+      break;
+    case "saturday":
+      console.log("Today is saturday");
+    default:
+      console.log("Invalid!");
+  }
+}
---
+++ javascript/inclass/sum_fun.js
@@ -0,0 +1,11 @@
+function sum(a, b) {
+  console.log("The sum is", a + b);
+}
+sum(5, 9);
+sum(99, 11);
+
+// Can be declare
+//  let sum= function(a,b)
+// {
+//     console.log("The sum is:",a+b);
+// }
