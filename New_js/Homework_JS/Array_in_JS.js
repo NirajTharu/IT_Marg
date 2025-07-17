30ff64f6611ac8513a468e65e37b355eb111b2ae
NirajTharu <nirajc279@gmail.com>
2025-07-17 07:47:21 +0545

    About the homework to be done

---
+++ New_js/Homework_JS/Array_in_JS.js
@@ -0,0 +1,45 @@
+// About the various operations of Array in Java Script
+
+// Array length(),Array delete(),Array toString(),Array concat(),
+// Array at(),Array copyWithin(),Array join(),Array flat(),Array pop(),Array slice(),Array push(),
+// Array splice(),Array shift(),Array toSpliced(),Array unshift();
+
+let array_A = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"];
+let array_a = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"];
+
+// For the length of the array A
+let A = array_A.length;
+let B = array_a.length;
+console.log(A);
+console.log(B);
+
+// For the delete the array
+let c = delete array_A[0];
+console.log(c);
+
+// Array toString
+
+// Array  concat(): to add the array to another array
+
+// Array at()
+
+// Array copyWithin()
+
+// Array Join()
+
+// Array flat()
+
+// Array pop()
+// Array push()
+
+// Array slice()
+// Array splice()
+// Array toSpliced()
+
+// Array shift()
+
+// For removing the last element of the array
+
+// For adding an element to the last of the array
+
+//
