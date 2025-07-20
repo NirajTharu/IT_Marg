8eddb913d70c2f9e2038530cfbe748881bf7dbe4
NirajTharu <nirajc279@gmail.com>
2025-07-20 15:30:16 +0545

    Various files were colaborated in a folder for the ease

--- New_js/Homework_JS/bg_color.js
+++ New_js/Homework_JS/bg_color.js
@@ -1,5 +1,10 @@
-background_color(){
-    let color_Change=document.getElementById("firstPara").value;
-
-    
-}
+// function color_Change() {
+//   let color_Name = document.getElementById("firstName").value;
+//   color_Name = document.getElementById("firstPara").style.backgroundColor;
+// }
+<body>
+  <p id="demo">Hello, Brave!</p>
+  <button onclick="document.getElementById('demo').style.backgroundColor = 'yellow'">
+    Change Background
+  </button>
+</body>;
